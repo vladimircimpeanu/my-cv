81bf5520c5048b11e67b88520292805381e3fb74
Vladimir Cimpeanu <vladimir.cimpeanu@yahoo.com>
2021-03-05 20:16:58 +0100

    update projects page

--- src/components/pages/projects/projects.js
+++ src/components/pages/projects/projects.js
@@ -1,19 +1,65 @@
 import React, { Component } from 'react';
 
-import { Tabs, Tab } from 'react-mdl';
-
 import Nature from '../../../assets/images/nature.jpg';
+import "./projects.css";
 
 class Projects extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { activeTab: 0 };
+
+    state = {
+        visible: true
     }
+
     render() {
         return (
-            <div>
 
-            </div >
+            <div className="body" style={{
+
+                height: '100%',
+                backgroundImage: "url(" + Nature + ")",
+                backgroundPosition: 'rightTop',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat'
+            }}>
+                <div className="tabs">
+                    <div onClick={() => { this.setState({ visible: !this.state.visible }) }} className="tab">JavaScript/React projects</div>
+                </div>
+                {this.state.visible ?
+                    <div className="cards">
+                        <div className="card">
+                            <div class="title">
+                                <h1>JavaScript project #1</h1>
+                            </div>
+                            <div className="line"></div>
+                            <div className="card-bottom"><div>In this project i made a login page of a movie website.</div>
+                                <a href="https://github.com/razvantimis/myIMDB_17.3/tree/main/pages/login_register">Github</a>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div class="title">
+                                <h1>JavaScript project #1</h1>
+                            </div>
+                            <div className="line"></div>
+                            <div className="card-bottom"><div>In this project i made a login page of a movie website.</div>
+                                <a href="https://github.com/razvantimis/myIMDB_17.3/tree/main/pages/login_register">Github</a>
+                            </div>
+                        </div>
+                    </div> :
+                    <div className="cards">
+                        <div className="card">
+                            <div class="title">
+                                <h1>React project #1</h1>
+                            </div>
+                            <div className="line"></div>
+                            <div className="react-card-bottom"><div>In this project i made this resume page</div>
+                                <a href="https://github.com/vladimircimpeanu/my-cv">Github</a>
+                            </div>
+                        </div>
+                    </div>
+
+
+                }
+            </div>
+
         )
     }
 }
